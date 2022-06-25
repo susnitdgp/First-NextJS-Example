6039e196745d68acec03398b7c1bb1d3bcc9e614
Susanta Goswami <susnitdgp@gmail.com>
2022-06-25 15:25:58 +0000

    About Modified

--- pages/About/index.js
+++ pages/About/index.js
@@ -1,10 +1,12 @@
-
+import { Divider } from "antd";
 
 function About(){
 
   return (
     <div>
         <h4>About Page</h4>
+        <Divider orientation="left">Antd Component</Divider>
+        <h4>About Page2</h4>
     </div>
 
   )
